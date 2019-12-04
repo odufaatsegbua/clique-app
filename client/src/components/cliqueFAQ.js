8a97a419294611aad0941ecc1e830a2740c3d6b0
Daniela Chavez <danielachavez18@gmail.com>
2019-12-03 18:26:53 -0600

    Styling changes to the fonts and colors

--- client/src/components/cliqueFAQ.js
+++ client/src/components/cliqueFAQ.js
@@ -80,10 +80,10 @@ export default class Community extends React.Component {
     <footer>
         <ul>
                 <a href="cliqueinfo.html"><li>About Us</li></a>
-                <a href=""><li>Contact</li></a>
+                <a href="contact.html"><li>Contact</li></a>
                 <a href="cliqueFAQ.html"><li>FAQ</li></a>
                 <a href="cliqueguidelines.html"><li>Community Guidelines</li></a>
-                <a href="#"><li>Legal</li></a>
+                <a href="NoMatch"><li>Legal</li></a>
         </ul>
         <p>Copyright &copy; Clique</p>
     </footer>
